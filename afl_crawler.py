207450beb9b2e3e83b401e6a03f986e569157190
zhao <zhao.wang.unsw@gmail.com>
2022-04-18 17:23:38 +0930

    data upgrade

--- afl_crawler.py
+++ afl_crawler.py
@@ -373,12 +373,12 @@ new_stats_data = stats.copy()
 new_games_data = games.copy()
 
 # Change the query round:
-new_stats_data, new_players_data, new_games_data = query_games_info(new_stats_data, new_players_data, new_games_data, 3,
-                                                                    4)
+new_stats_data, new_players_data, new_games_data = query_games_info(new_stats_data, new_players_data, new_games_data, 4,
+                                                                    5)
 
 new_stats_data.to_csv('./output/stats.csv', index=False)
 new_players_data.to_csv('./output/players.csv', index=False)
 new_games_data.to_csv('./output/games.csv', index=False)
 
-query_round = 5
+query_round = 6
 query_prediction_format(query_round)
